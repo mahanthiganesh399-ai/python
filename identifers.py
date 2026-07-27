58d80305ec3a0525e2b9bee514959dae6227cadb
mahanthi ganesh <mahanthiganesh399@gmail.com>
2026-07-27 12:15:00 +0530

    Initial commit

---
+++ identifers.py
@@ -0,0 +1,13 @@
+
+age = 19
+MAX_MARKS = 100
+def greet():
+    return "Hello!"
+class Student:
+    pass
+student_name = "Ganesh"
+print("Variable:", age)
+print("Constant-style name:", MAX_MARKS)
+print("Function name:", greet())
+print("Class name:", Student)
+print("Name using an underscore:", student_name) 
